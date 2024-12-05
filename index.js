fa0987472c791393a341e346249ec05c87e23b44
Phil Palmarozzo <philip.palmarozzo@affiliated.com>
2024-12-05 13:58:35 -0500

    Added date check, function return conditions

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // This program will navigate to the Hacker News website and verify that the articles are sorted from newest to oldest
-
 const { chromium } = require("playwright");
 
 async function validateArticles() {
@@ -14,12 +13,11 @@ async function validateArticles() {
   // Give the page time to load
   await page.waitForSelector(".athing");
 
-  // Declare body of main table
+  // Locate body of main table
   const body = await page.locator("table#hnmain tbody tr td table");
 
-  // Get number of rows in the table and print total
+  // Get number of rows in the table
   const totalRows = await body.locator("tr").count();
-  console.log(totalRows);
 
   // Locate the table containing the articles
   const tableLocator = await page.locator("table#hnmain tbody tr td table");
@@ -27,11 +25,15 @@ async function validateArticles() {
   // Create array for storing datetimes
   let ages = new Array();
 
+  // (TESTING USE ONLY) Force article ages to be OUT OF ORDER
+  //const dateTest = new Date("2024-12-05T06:50:13Z");
+  //ages.push(dateTest);
 
+  // Check if age array is full
   while (ages.length < 100) {
+
     // Loop over each row, starting with the second row (Skipping the header)
     for (let i = 1; i <= totalRows; i++) {
-      //DELETE const second_row_text = await tableLocator.locator("tr").nth(i).locator(":scope").allInnerTexts();
 
       // Locate the nth row, columns in the row, and get the # of columns
       const rowLocator = await tableLocator.locator("tr").nth(i);
@@ -48,30 +50,44 @@ async function validateArticles() {
         // Verify span element exists in the column
         const spanExists = await spanLocator.count() > 0;
 
-        // Add article age to array
+        // Add article age to array only if there are less than 100 elements
         if (spanExists && ages.length < 100) {
 
           // Extract the age of the post from the 'title' attribute, convert to datetime & add to array
           const age = await spanLocator.getAttribute("title");
           const dateObject = new Date(age.substring(0, 19) + "Z");
           ages.push(dateObject);
+
+          // When there is more than one age in the array, check if it is in the correct order
+          if ((ages.length > 0) && (ages[ages.length - 1] > ages[ages.length - 2])) {
+
+            // Close browser and return false if articles are out of order
+            await browser.close();
+            return false;
+          };
         } else {
+
+          // Continue running if span element does not exist
           continue;
-        }
-      }
-    }
+        };
+      };
+    };
 
+    // Go to the next page
     await page.locator("a.morelink").click();
-
-  }
-
-  console.log(ages);
+  };
   
-
-  // Close browser
+  // Close browser and return true when articles are in order
   await browser.close();
-}
+  return true;
+};
 
 (async () => {
-  await validateArticles();
+
+  // Print results to console
+  if (await validateArticles()) {
+    console.log("Articles are in order.");
+  } else {
+    console.log("Articles are out of order.");
+  };
 })();
